cb473f3d47e244aed7908db583d1488bd545f309
kevd4t <k.blanco@d4t.tech>
2023-06-19 11:55:29 -0400

    adding meta data into _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document () {
   return (
     <Html lang='es' suppressHydrationWarning>
-      <Head />
+      <Head>
+        <meta name='robots' content='noindex,nofollow' />
+        <meta charSet='UTF-8' />
+        <meta name='description' content='ERP - D4T Platform' />
+        <meta name='author' content='D4T Technologies' />
+        <meta name='copyright' content='D4T Technologies' />
+        <meta httpEquiv='Content-Security-Policy' content='upgrade-insecure-requests' />
+
+      </Head>
+
       <body>
         <Main />
         <NextScript />
