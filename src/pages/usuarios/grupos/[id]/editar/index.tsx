91527f3f61869fc97102482ef1d78ff552945c37
kevd4t <k.blanco@d4t.tech>
2023-07-06 10:36:11 -0400

    adding user groups pages

---
+++ src/pages/usuarios/grupos/[id]/editar/index.tsx
@@ -0,0 +1,50 @@
+import { useRouter } from 'next/router'
+
+import type { IFetchData, IUserDetail, ReactElement } from '@/lib/types'
+import { useFetch } from '@/lib/hooks/useFetch'
+import { siteConfig } from '@/config'
+
+import { AuthenticatedLayout } from '@/layouts/Authenticated'
+import { WomanLoading } from '@/components/common/illustrations/WomanLoading'
+import { HeaderPage } from '@/components/common/headers/HeaderPage'
+
+const { ROUTES } = siteConfig
+
+const EditUserGroupPage = () => {
+  const router = useRouter()
+  const { error, isLoading } = useFetch<IFetchData<IUserDetail>>(`/api/users/${router.query.id}`)
+
+  if (error) {
+    return (
+      <div className='w-full h-[80vh] flex flex-col justify-center items-center'>
+        <WomanLoading className='w-96' />
+        <h5 className='font-bold text-4xl'>Hubo un Error</h5>
+      </div>
+    )
+  }
+
+  if (isLoading) {
+    return (
+      <div className='w-full h-[80vh] flex flex-col justify-center items-center'>
+        <WomanLoading className='w-96' />
+        <h5 className='font-bold text-4xl'>Cargando...</h5>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <HeaderPage title={`Editar Usuario ${router.query.id}`} />
+    </>
+  )
+}
+
+EditUserGroupPage.getLayout = function getLayout (page: ReactElement) {
+  return (
+    <AuthenticatedLayout title={`${ROUTES.USERS.DETAIL.TITLE} | ${siteConfig.TITLE}`} >
+      {page}
+    </AuthenticatedLayout>
+  )
+}
+
+export default EditUserGroupPage
