cc6fbeb919516e94cd6cb139bb4511ccb5f3c6b3
kevd4t <k.blanco@d4t.tech>
2023-08-03 08:38:19 -0400

    init project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components'
+
+export default function Home() {
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+      <Button>Click me</Button>
+    </div>
+  )
+}
