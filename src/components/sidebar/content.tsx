0b4367eb3048e9662647024a8aed62a9649c93e0
kevd4t <k.blanco@d4t.tech>
2023-08-14 17:01:27 -0400

    fix sidebar styles

--- src/components/sidebar/content.tsx
+++ src/components/sidebar/content.tsx
@@ -1,9 +1,13 @@
 'use client'
 
+import { useSidebar } from "./"
+
 export const SidebarContent = ({ children }) => {
+  const { isExpanded } = useSidebar()
+
   return (
     <section className='w-full h-full py-3 scroll-content'>
-      <div className={`w-full space-y-3 pr-2`}>
+      <div className={`${isExpanded ? ' pr-2' : 'pl-2'} w-full space-y-3`}>
         {children}
       </div>
     </section>
