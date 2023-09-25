4a634eb0264f4452e302facea2a44cf59ecd5018
kevd4t <k.blanco@d4t.tech>
2023-09-25 09:07:23 -0400

    change icon color uploadimage

--- src/components/upload-image/SingleImage/Actions.tsx
+++ src/components/upload-image/SingleImage/Actions.tsx
@@ -26,7 +26,7 @@ export const UploadImageActions = ({ imageIndex, compress, tabIndexs, onImageUpd
             variant='outline'
             tabIndex={tabIndexs?.viewCompress}
             onClick={() => compress.openComparisons()}
-            className='dark:text-white text-green-600 backdrop-blur-sm border-green-600 bg-green-600 bg-opacity-10 hover:bg-green-600 hover:bg-opacity-25 hover:text-green-600'
+            className='text-green-600 backdrop-blur-sm border-green-600 bg-green-600 bg-opacity-10 hover:bg-green-600 hover:bg-opacity-25 hover:text-green-600'
           >
             <IconPhotoStar size={16} />
           </Button>
@@ -39,7 +39,7 @@ export const UploadImageActions = ({ imageIndex, compress, tabIndexs, onImageUpd
         variant='outline'
         tabIndex={tabIndexs?.change}
         onClick={() => onImageUpdate(imageIndex)}
-        className='dark:text-white text-yellow-600 backdrop-blur-sm border-yellow-600 bg-yellow-600 bg-opacity-10 hover:bg-yellow-600 hover:bg-opacity-25 hover:text-yellow-600'
+        className='text-yellow-600 backdrop-blur-sm border-yellow-600 bg-yellow-600 bg-opacity-10 hover:bg-yellow-600 hover:bg-opacity-25 hover:text-yellow-600'
       >
         <Edit size={14} />
       </Button>
@@ -54,7 +54,7 @@ export const UploadImageActions = ({ imageIndex, compress, tabIndexs, onImageUpd
           setLocalImage([])
           setUploadImage({ original: null, compressed: null })
         }}
-        className='dark:text-white text-red-700 backdrop-blur-sm border-red-700 bg-red-700 bg-opacity-10 hover:bg-red-700 hover:bg-opacity-25 hover:text-red-700'
+        className='text-red-700 backdrop-blur-sm border-red-700 bg-red-700 bg-opacity-10 hover:bg-red-700 hover:bg-opacity-25 hover:text-red-700'
       >
         <Trash size={14} />
       </Button>
