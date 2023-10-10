625acd4f63d8bfd65d0cb1ee1c9417b9f8ba99f2
kevd4t <k.blanco@d4t.tech>
2023-10-10 15:49:44 -0400

    feature: update version + build

--- dist/src/components/sidebar/nav-link.d.ts
+++ dist/src/components/sidebar/nav-link.d.ts
@@ -1,10 +1,11 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 declare type LinkNavigationProps = {
     to: string;
     label: string;
     icon?: ReactNode;
     pathname: string;
     Link?: any;
+    isBottomNavLink?: boolean;
 };
-export declare const NavLink: ({ to, label, icon, pathname, Link }: LinkNavigationProps) => JSX.Element;
+export declare const NavLink: ({ to, label, icon, pathname, Link, isBottomNavLink, }: LinkNavigationProps) => JSX.Element;
 export {};
