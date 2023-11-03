ca9a0ed33b12072b22ebe168ddd3717501f2ee32
Cristian Sanchez <c.sanchez@d4t.tech>
2023-11-03 11:38:39 -0400

    fix: :ambulance: Solve issue with link into bottom nav buttons (add paths references)

--- src/components/bottom-navigation/index.tsx
+++ src/components/bottom-navigation/index.tsx
@@ -24,6 +24,7 @@ export function BottomNavigation({
             icon={item.icon}
             titleDescription={item.titleDescription}
             Link={Link}
+            item={item}
           />
         ))}
 
@@ -32,6 +33,7 @@ export function BottomNavigation({
           titleDescription="Options"
           actionToSet={setIsDrawerOpen}
           Link={null}
+          item={null}
         />
       </div>
 
