f575b3686917a0b4d6b159f78649074a7ef51945
kevd4t <k.blanco@d4t.tech>
2023-12-21 15:58:29 -0400

    build + update version

--- src/components/scroll-area.tsx
+++ src/components/scroll-area.tsx
@@ -7,7 +7,7 @@ import { cn } from '../lib/utils'
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
+  Omit<React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>, 'placeholder'>
 >(({ className, children, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     ref={ref}
