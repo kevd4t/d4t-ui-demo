80d8410b9486511a2c195b1009635a3970b0c7ee
kevd4t <k.blanco@d4t.tech>
2024-02-23 10:57:59 -0400

    build + update version

--- dist/src/components/upload-image/SingleImage/LoadImage.d.ts
+++ dist/src/components/upload-image/SingleImage/LoadImage.d.ts
@@ -1,9 +1,10 @@
 /// <reference types="react" />
-export declare const LoadImage: ({ dragProps, isDragging, emptyClassName, tabIndexs, uploadLabel, onImageUpload }: {
+export declare const LoadImage: ({ dragProps, isDragging, emptyClassName, tabIndexs, uploadLabel, disabled, onImageUpload }: {
     dragProps: any;
     isDragging: any;
     emptyClassName: any;
     tabIndexs: any;
     uploadLabel: any;
+    disabled: any;
     onImageUpload: any;
 }) => JSX.Element;
