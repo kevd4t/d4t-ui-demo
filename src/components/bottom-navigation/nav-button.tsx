289f063451d5a450406fd933fa5c21b1fe394411
Cristian Sanchez <c.sanchez@d4t.tech>
2024-04-01 10:18:41 -0400

    fix: :bug: Change test reference into sidebar button

--- src/components/bottom-navigation/nav-button.tsx
+++ src/components/bottom-navigation/nav-button.tsx
@@ -31,6 +31,7 @@ export default function BottomNavigationButton({
                 }
                 : null
             }
+            data-testId={`bottom-sidebar-${titleDescription}`}
             role="button"
             className="w-full border-2 border-transparent flex justify-center items-center p-2 group group-hover:text-[#eaeaea] text-base font-normal text-brand-sidebar-text rounded-lg hover:bg-brand-sidebar-iconsHover hover:dark:bg-main-hover select-none"
           >
