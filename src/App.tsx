a706deb3e5622dcb7afb5bff4be73dcc563bc6a6
kevd4t <k.blanco@d4t.tech>
2024-11-13 16:11:46 -0400

    add: sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -128,184 +128,15 @@ function App() {
   return (
     <ThemeProvider attribute='class' defaultTheme='light'>
       <AppLayout>
-        {/* Sidebar */}
-        <Sidebar
-          logout={() => { }}
-          profile={profile}
-          theme={{ toggleTheme: () => toggleThemeFunc(), value: theme }}
-        >
-          <SidebarContent>
-            <NavLink
-              to='/asd'
-              pathname='/asd'
-              label='Esto es un label muy largo asi que se va a cortar'
-              icon={<Settings className='dark:text-white' size={20} />}
-            />
 
-            <NavLinkNested
-              pathname='/fino'
-              label='Todo bien mi compa todo finoo aaaaa'
-              icon={<Settings className='dark:text-white' size={20} />}
-              sublinks={[
-                {
-                  label:
-                    'Ajaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa asd asfsdf sdf sdgfds',
-                  to: '/fino',
-                  icon: <User className='dark:text-white' />,
-                },
-                {
-                  label: 'ecole x 2',
-                  to: '/ecole',
-                  icon: <User className='dark:text-white' />,
-                },
-                {
-                  label: 'ecole x 3',
-                  to: '/ecol3',
-                  icon: <User className='dark:text-white' />,
-                },
-                {
-                  label: 'ecole x 4',
-                  to: '/ecol4',
-                  icon: <User className='dark:text-white' />,
-                },
-                {
-                  label: 'ecole x 5',
-                  to: '/ecol5',
-                  icon: <User className='dark:text-white' />,
-                },
-              ]}
-            />
-
-            <NavLink
-              to='/ja'
-              pathname='/'
-              label='Eventos'
-              icon={<Settings className='dark:text-white' size={20} />}
-            />
-
-            <NavLink
-              to='/xd'
-              pathname='/'
-              label='Lo que sea'
-              icon={<Settings className='dark:text-white' size={20} />}
-            />
-
-            <NavLink
-              to='/xd2'
-              pathname='/'
-              label='Lo que sea2'
-              icon={<Settings className='dark:text-white' size={20} />}
-            />
-
-            <NavLinkNested
-              pathname='/fino'
-              label='Epa'
-              icon={<Settings className='dark:text-white' size={20} />}
-              sublinks={[
-                {
-                  label: 'Aja',
-                  to: '/fino',
-                  icon: <User className='dark:text-white' />,
-                },
-                {
-                  label: 'ecole x 2',
-                  to: '/ecole',
-                  icon: <User className='dark:text-white' />,
-                },
-                {
-                  label: 'ecole x 3',
-                  to: '/ecol3',
-                  icon: <User className='dark:text-white' />,
-                },
-                {
-                  label: 'ecole x 4',
-                  to: '/ecol4',
-                  icon: <User className='dark:text-white' />,
-                },
-                {
-                  label: 'ecole x 5',
-                  to: '/ecol5',
-                  icon: <User className='dark:text-white' />,
-                },
-              ]}
-            />
-            <NavLink
-              to='/xd3'
-              pathname='/'
-              label='Lo que sea3'
-              icon={<Settings className='dark:text-white' size={20} />}
-            />
-
-            <NavLinkNested
-              label='Ajustes'
-              pathname='/'
-              icon={<Settings className='dark:text-white' size={20} />}
-              sublinks={[
-                {
-                  label: 'Ubicaciones',
-                  to: '/ajustes/ubicaciones/estados',
-                  icon: <Building className='dark:text-white' size={20} />,
-                },
-                {
-                  label: 'Categorias',
-                  to: '/ajustes/categorias',
-                  icon: <HelpCircle className='dark:text-white' size={20} />,
-                },
-                {
-                  label: 'Estados',
-                  to: '/ajustes/estados',
-                  icon: <BarChart className='dark:text-white' size={20} />,
-                },
-                {
-                  label: 'Choferes',
-                  to: '/ajustes/choferes',
-                  icon: <Truck className='dark:text-white' size={20} />,
-                },
-                {
-                  label: 'Flotas',
-                  to: '/ajustes/flotas',
-                  icon: <StopCircle className='dark:text-white' size={20} />,
-                },
-                {
-                  label: 'Marcas de Medidores',
-                  to: '/ajustes/marcas-de-medidores',
-                  icon: <Badge className='dark:text-white' size={20} />,
-                },
-                {
-                  label: 'Medidores',
-                  to: '/ajustes/medidores',
-                  icon: <Router className='dark:text-white' size={20} />,
-                },
-                {
-                  label: 'Marcas de GPS',
-                  to: '/ajustes/marcas-de-gps',
-                  icon: <LucideTruck className='dark:text-white' size={20} />,
-                },
-                {
-                  label: 'Dispositivos GPS',
-                  to: '/ajustes/dispositivos-gps',
-                  icon: <DivideCircle className='dark:text-white' size={20} />,
-                },
-              ]}
-            />
-
-            <NavLink
-              to='/almacenamiento'
-              pathname='/'
-              label='Almacenamiento'
-              icon={<Settings className='dark:text-white' size={20} />}
-            />
-          </SidebarContent>
-        </Sidebar>
-
-        <div className='w-full'>
+        <div className='w-full bg-card rounded-md  p-4'>
           <Button onClick={() => openDialog()}>
             Agregar Carrier
           </Button>
           <FormCreateGPS />
         </div>
 
-        <Dialog>
+        {/* <Dialog>
           <DialogTrigger>
             <Info size={18} className='mr-2' />
             Mas Info
@@ -322,9 +153,9 @@ function App() {
               <TabsContent value="password">Change your password here.</TabsContent>
             </Tabs>
           </DialogContent>
-        </Dialog>
+        </Dialog> */}
 
-        <div className='grid'>
+        <div className='grid bg-card mt-4 rounded-md p-4'>
           <div className='grid grid-cols-2'>
             {/* Forms */}
             <div className='m-10'>
--- src/layouts/Application.tsx
+++ src/layouts/Application.tsx
@@ -1,7 +1,35 @@
+import { BreadcrumbPage, Separator, Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator } from '../components'
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '../components/sidebar'
+import { AppSidebar } from '../examples/sidebar'
+
 export const AppLayout = ({ children }) => {
   return (
-    <div className='app-layout'>
-      {children}
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+
+      <SidebarInset className='p-4'>
+        <header className="sticky top-0 left-0 flex h-16 shrink-0 items-center gap-2 border-b px-4 bg-card rounded-md z-10 w-full max-w-5xl mx-auto">
+          <SidebarTrigger className="-ml-1 z-10 rounded-full hover:bg-blue-500/20" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+
+        <div className='p-4 rounded-md mt-2 max-w-6xl mx-auto'>
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
